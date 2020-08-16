6a2d8314acdb6762cff593f3e07bff5561c4f1ad
Valeria Oshiro <valeriaoshiro19@gmail.com>
2020-08-15 19:23:09 -0700

    Created details and added route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="details/:id" />
+      </Router>
     </div>
   );
 };
